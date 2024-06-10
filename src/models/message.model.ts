11f5cf52ae4d1a72b449386e29dca236f0b4a3ab
Subham Mani <manisubham09@gmail.com>
2024-06-10 12:32:17 +0530

    add message and user model

---
+++ src/models/message.model.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface Message extends Document {
+  content: string;
+  createdAt: Date;
+}
+
+const messageSchema: Schema<Message> = new Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now(),
+  },
+});
+
+export const MessageModel =
+  (mongoose.models.Message as mongoose.Model<Message>) ||
+  mongoose.model<Message>("Message", messageSchema);
